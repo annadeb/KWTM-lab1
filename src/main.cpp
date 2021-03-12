5bda4c36c80c4a654b94fa6cc370f4bb5bc39e24
Anna Dębowska <anna-debowska@o2.pl>
2021-03-12 16:29:21 +0100

    Commit 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
-#include <trygonometria.h>
+#include<LaborkaConfig.h>
 
-int main() { 
+#ifdef USE_TRIGONOMETRY_DEGREE
+#include<trygonometria.h>
+#else
+#include<cmath>
+#endif
+#include <iostream>
 
-	double sinus = degreemath::sin(30.0);
-	double cosinus = degreemath::cos(30.0);
-	double tangens = degreemath::tg(30.0);
-	double cotangens = degreemath::ctg(30.0);
+int main() {
+	double sinus;
+	double cosinus;
+	double tangens;
+	double cotangens;
 
-	return 0; 
+#ifdef USE_TRIGONOMETRY_DEGREE
+	 sinus = degreemath::sin(30.0);
+	 cosinus = degreemath::cos(30.0);
+	 tangens = degreemath::tg(30.0);
+	 cotangens = degreemath::ctg(30.0);
+#else
+	sinus = std::sin(pi/4);
+	cosinus = std::cos(pi/4);
+	tangens = std::tg(pi/4);
+	cotangens = 1 / std::tg(pi/4);
+#endif
+
+	std::cout << sinus << std :: endl;
+
+	return 0;
 }
